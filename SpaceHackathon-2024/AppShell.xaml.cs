92125b44b9ac7d7844a3d31f057fad1bd7da89b0
Nikita Kharashun <tsurai7walkin@gmail.com>
2024-05-22 23:00:51 +0300

    Add chat redirection

--- SpaceHackathon-2024/AppShell.xaml.cs
+++ SpaceHackathon-2024/AppShell.xaml.cs
@@ -11,6 +11,12 @@ namespace SpaceHackathon_2024
             Routing.RegisterRoute("SignUpPage", typeof(SignUpPage));
             Routing.RegisterRoute("SignInPage", typeof(SignInPage));
             Routing.RegisterRoute("ProfilePage", typeof(ProfilePage));
+            Routing.RegisterRoute("ChatPage", typeof(ChatPage));
+        }
+        
+        private async void OnEnvelopeTapped(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync($"ChatPage");
         }
     }
 }
