5e0fe7d4dd69f8705474816abcfff1d254d5d0fd
Wendermark <caseharo.e@gmail.com>
2024-05-22 13:56:55 +0300

    test news page upload

---
+++ SpaceHackathon-2024/Models/News.cs
@@ -0,0 +1,11 @@
+﻿namespace SpaceHackathon_2024.Models
+{
+    public class News(string title, string description, string imageURL)
+    {
+        public string Title { get; private set; } = title;
+
+        public string Description { get; private set; } = description;
+
+        public string ImageURL { get; private set; } = imageURL;
+    }
+}
---
+++ SpaceHackathon-2024/ViewModels/NewsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using SpaceHackathon_2024.Models;
+
+namespace SpaceHackathon_2024.ViewModels
+{
+    public partial class NewsViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private List<News> _news = new() { new("test", "test", "test"), new("test", "test", "test"), new("test", "test", "test") };
+    }
+}
