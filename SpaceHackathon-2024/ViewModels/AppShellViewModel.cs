d61a8238b25108408c10ba09e04cdb012cc511c3
robilkot <timur.robilko@gmail.com>
2024-05-22 21:34:29 +0200

    Markup fixes

---
+++ SpaceHackathon-2024/ViewModels/AppShellViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SpaceHackathon_2024.ViewModels
+{
+    public class AppShellViewModel : ObservableObject
+    {
+        public async void OpenNewsPage()
+        {
+            //await Shell.Current.GoToAsync("NewsPage", true, )
+        }
+    }
+}
--- SpaceHackathon-2024/ViewModels/ProfileViewModel.cs
+++ SpaceHackathon-2024/ViewModels/ProfileViewModel.cs
@@ -1,8 +1,32 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace SpaceHackathon_2024.ViewModels;
 
-public class ProfileViewModel : ObservableObject
+public partial class ProfileViewModel : ObservableObject
 {
-    
+    [ObservableProperty]
+    private bool _workInfoExpanded;
+    [ObservableProperty]
+    private double _workInfoExpandedRotation = 90;
+
+
+    [ObservableProperty]
+    private bool _ratingExpanded;
+    [ObservableProperty]
+    private double _ratingExpandedRotation = 90;
+
+    [RelayCommand]
+    public void WorkInfoHeaderTapped()
+    {
+        WorkInfoExpanded = !WorkInfoExpanded;
+        WorkInfoExpandedRotation = WorkInfoExpanded ? 270 : 90;
+    }
+
+    [RelayCommand]
+    public void RatingHeaderTapped()
+    {
+        RatingExpanded = !RatingExpanded;
+        RatingExpandedRotation = RatingExpanded ? 270 : 90;
+    }
 }
