b730637542ba5ea41d1946f372f67a885776470d
Wendermark <caseharo.e@gmail.com>
2024-05-22 17:44:51 +0300

    News: Publish Date Added

--- SpaceHackathon-2024/Models/News.cs
+++ SpaceHackathon-2024/Models/News.cs
@@ -4,12 +4,14 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace SpaceHackathon_2024.Models
 {
     [PrimaryKey("Id")]
-    public class News(string title, string description, string imageURL)
+    public class News(string title, string publishDate, string description, string imageURL)
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         public string Title { get; private set; } = title;
+
+        public string PublishDate { get; private set; } = publishDate;
 
         public string Description { get; private set; } = description;
 
