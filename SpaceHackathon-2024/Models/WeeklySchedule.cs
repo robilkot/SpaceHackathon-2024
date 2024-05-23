cc1db250033461665bffde65ac4fc857e81b1312
Егор Гоков <caseharo.e@gmail.com>
2024-05-24 02:31:21 +0300

    database fix

--- SpaceHackathon-2024/Models/WeeklySchedule.cs
+++ SpaceHackathon-2024/Models/WeeklySchedule.cs
@@ -1,7 +1,13 @@
-﻿namespace SpaceHackathon_2024.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SpaceHackathon_2024.Models
 {
+    [PrimaryKey("Id")]
     public class WeeklySchedule
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
         public DateOnly StartDay { get; set; }
         public DateOnly EndDay { get; set; }
         public List<ScheduleDay> Days { get; set; }
